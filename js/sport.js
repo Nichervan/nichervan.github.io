29dbbabbd725c19c0042b19c5f27d3137db7adbb
Nichervan <nichervan.brhik@gmail.com>
2017-04-12 10:13:15 +0300

    added paragraph About me

--- js/sport.js
+++ js/sport.js
@@ -1,13 +1,34 @@
 $(document).ready(function(){
-	$()
+	SportNews();
 });
+		// url:'https://newsapi.org/v1/articles?source=sky-sports-news&sortBy=latest&apiKey=329824d689004139b48d23d6386b1238',
 
-function SportNews() {
-	$.ajax({
-		url:'https://newsapi.org/v1/articles?source=sky-sports-news&sortBy=latest&apiKey=329824d689004139b48d23d6386b1238',
-		type: 'GET',
-		success: function (sport) {
+		function SportNews() {
+			$.ajax({
+				
+				url:'https://newsapi.org/v1/articles?source=bbc-sport&apiKey=329824d689004139b48d23d6386b1238',
+				type: 'GET',
+				success: function (sport) {
 
-			handleRresult(sport);
+					handleSport(sport);
+				}
+			});
 		}
-	})
+		
+		function handleSport(sport){
+			var html='<ul>';
+			$.each(sport.articles,function(index,item){
+				html+=`<li> <div><img width="300" height="300" src="${item.urlToImage}" alt="" /></div></br>`;
+				html+=`<a href="${item.url}" target="_blank" >${item.title}</a>`;  
+
+				
+				
+				html+=`<p>"${item.description}"</p>`;
+				html+=`<h6>"${item.author}"</h6>`;
+				html+=`<h5>"${item.publishedAt}"</h5></li>`;
+			})
+			html+='</ul>';
+
+			$('#result-sports').html(html);
+
+		}
